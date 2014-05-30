92257d5c80201f025e9a0c85cc5a9458f592ec46
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-30 16:55:55 -0400

    Add the index.js file

---
+++ index.js
@@ -0,0 +1,90 @@
+var CameraApp = function(){
+  var self = this;
+  this.pictureSource;   // picture source
+  this.destinationType; // sets the format of returned value
+
+  // Wait for device API libraries to load
+  document.addEventListener("deviceready", self.onDeviceReady,false);
+};
+
+
+// device APIs are available
+//
+CameraApp.prototype.onDeviceReady = function () {
+    this.pictureSource    = navigator.camera.PictureSourceType;
+    this.destinationType  = navigator.camera.DestinationType;
+}
+
+// Called when a photo is successfully retrieved
+//
+CameraApp.prototype.onPhotoDataSuccess = function(imageData) {
+  // Uncomment to view the base64-encoded image data
+  // console.log(imageData);
+
+  // Get image handle
+  //
+  var smallImage = document.getElementById('smallImage');
+
+  // Unhide image elements
+  //
+  smallImage.style.display = 'block';
+
+  // Show the captured photo
+  // The in-line CSS rules are used to resize the image
+  //
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// Called when a photo is successfully retrieved
+//
+CameraApp.prototype.onPhotoURISuccess = function(imageURI) {
+  // Uncomment to view the image file URI
+  // console.log(imageURI);
+
+  // Get image handle
+  //
+  var largeImage = document.getElementById('largeImage');
+
+  // Unhide image elements
+  //
+  largeImage.style.display = 'block';
+
+  // Show the captured photo
+  // The in-line CSS rules are used to resize the image
+  //
+  largeImage.src = imageURI;
+}
+
+// A button will call this function
+//
+CameraApp.prototype.capturePhoto = function() {
+  // Take picture using device camera and retrieve image as base64-encoded string
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+    destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+//
+CameraApp.prototype.capturePhotoEdit = function() {
+  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+    destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+//
+CameraApp.prototype.getPhoto = function(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+    destinationType: destinationType.FILE_URI,
+    sourceType: source });
+}
+
+// Called if something bad happens.
+//
+CameraApp.prototype.onFail = function(message) {
+  setTimeout(function(){
+    alert('Failed because: ' + message);
+  }, 0);
+}
+
